6c46656610026ea88a81c413d64d6eb56c849b12
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-04-06 20:42:16 -0500

    Test Download Button

---
+++ jspdf.js
@@ -0,0 +1,15 @@
+window.jsPDF = window.jspdf.jsPDF;
+var doc = new jsPDF();
+    var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+            return true;
+        }
+    };
+
+    $('#cmd').click(function () {
+        doc.fromHTML($('#content').html(), 15, 15, {
+            'width': 170,
+                'elementHandlers': specialElementHandlers
+        });
+        doc.save('sample-file.pdf');
+    });
